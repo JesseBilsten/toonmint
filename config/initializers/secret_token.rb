5e114b99becc032df9debe8e8ec6893af6fa275e
Jesse Bilsten <jesse@bilsten.net>
2012-09-06 15:41:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToonMint::Application.config.secret_token = 'ff755fe583eaf2f6a0fd858a97986dbcdcb5c546925def524bfc85fd459bbedb251a520c3cdbf0c6adb3c19bf09018ee07fd6b92e772c26928d25c2d4d68d1de'
